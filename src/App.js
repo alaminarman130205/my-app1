fd016f2637d03961002a15b9034bc34fd9d0e52e
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-09-19 22:28:19 +0600

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+const number = 555;
+const singer = {name:'dr asik',job: 'singer'}
+const singer1 = {name:'jakir nayek', job:"scoller"}
+const singerStyle = {
+  color : 'purple', 
+  backgroundColor:'green',
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
     </div>
   );
+}
+function Person(){
+  return (
+
+      <div>
+          <h1>royel bengol tiger</h1>
+          <p>this is inside the div</p>
+      </div>
+
+  )
 }
 
 export default App;
